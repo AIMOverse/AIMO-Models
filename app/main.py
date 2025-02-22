67491b0c5553994ed4bfad2fa431b725a6c13214
dcloud347 <17864201683@163.com>
2025-02-22 17:24:15 +0000

    Improve logging configuration and enhance comments for clarity in main.py

--- app/main.py
+++ app/main.py
@@ -16,32 +16,36 @@ Description:
     This is the controller of the app.
 """
 
-# logging configuration
+# Configure logging settings
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-
 def custom_generate_unique_id(route: APIRoute) -> str:
+    """
+    Custom function to generate unique IDs for API routes based on their tags and names.
+    """
     return f"{route.tags[0]}-{route.name}"
 
 # Initialize the FastAPI application
 app = FastAPI(
-    title=settings.PROJECT_NAME, # Title on generated document
-    openapi_url=f"{settings.API_V1_STR}/openapi.json", # generated document path
-    generate_unique_id_function=custom_generate_unique_id,
-    version=settings.version
+    title=settings.PROJECT_NAME,  # Title on generated documentation
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",  # Path to generated OpenAPI documentation
+    generate_unique_id_function=custom_generate_unique_id,  # Custom function for generating unique route IDs
+    version=settings.version  # Version of the API
 )
 
 # Configure CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
+    allow_origins=settings.BACKEND_CORS_ORIGINS,  # Allowed origins for CORS
+    allow_credentials=True,  # Allow credentials in CORS
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"]  # Allow all headers
 )
 
+# Include the API router with a prefix
 app.include_router(api_router, prefix=settings.API_V1_STR)
+# Include the API router without a prefix
 app.include_router(api_router)
