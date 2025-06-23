345ac2574eca9b5ca54a80d326751886befa9a42
Wes1ey <wesley.xu.23@ucl.ac.uk>
2025-06-24 00:56:36 +0100

    feat(db): simplify database engine creation by removing conditional DATABASE_URL logic

--- app/core/db.py
+++ app/core/db.py
@@ -2,14 +2,8 @@ from sqlmodel import create_engine, SQLModel
 
 from app.core.config import settings
 
-import os
+engine = create_engine(settings.DATABASE_URL, echo=True)
 
-if os.environ.get("TESTING") == "True":
-    DATABASE_URL = "sqlite:///:memory:"
-else:
-    DATABASE_URL = settings.DATABASE_URL
-
-engine = create_engine(DATABASE_URL)
 
 # Create the database and tables
 def create_db_and_tables():
